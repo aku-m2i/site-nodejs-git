323df5f31c9b3e17f2fb0e5add709bda547d4917
aku-m2i <akummar.aghali@labom2iformation.fr>
2025-09-17 15:59:31 +0200

    Ajout Node.js app

---
+++ server.js
@@ -0,0 +1,7 @@
+ const express = require("express");
+ const app = express();
+ const port = 8080;
+ app.get("/", (req, res) => {
+ res.send("<h1>Hello depuis OpenShift (GitHub direct)!</h1>");
+ });
+ app.listen(port, () => console.log(`App running on port ${port}`));
