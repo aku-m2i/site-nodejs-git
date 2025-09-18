179f5362073036b0d5e122a68ec827a217049aa4
aku-m2i <akummar.aghali@labom2iformation.fr>
2025-09-18 09:32:01 +0200

    app Node.js modifiée

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
  const app = express();
  const port = 8080;
  app.get("/", (req, res) => {
- res.send("<h1>Hello depuis OpenShift (GitHub direct)!</h1>");
+ res.send('<h1 style="color:Red;">Bienvenue depuis mon app Node.js modifiée</h1><p style="color:purple;">Page mise à jour</p>');
  });
  app.listen(port, () => console.log(`App running on port ${port}`));
